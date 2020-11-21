acda9380d1d66896f1cc036aae34146b83e5a54a
Andrew Gregory <andrew.gregory.8@gmail.com>
2020-11-21 15:50:03 -0800

    update copyright and license information
    Signed-off-by: Andrew Gregory <andrew.gregory.8@gmail.com>

--- globdir.c
+++ globdir.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 Andrew Gregory <andrew.gregory.8@gmail.com>
+ * Copyright 2017-2020 Andrew Gregory <andrew.gregory.8@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
